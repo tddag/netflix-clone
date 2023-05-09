b9386a8a6642ddd10cee2fd6430fc728d9a08e65
tddag <danhtam0509@gmail.com>
2023-05-09 00:14:43 -0400

    finished Info Modal component

--- components/AccountMenu.tsx
+++ components/AccountMenu.tsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import { signOut } from 'next-auth/react';
+import useCurrentUser from '../hooks/useCurrentUser';
 
 interface AccountMenuProps {
   visible?: boolean;
 }
 
 const AccountMenu: React.FC<AccountMenuProps> = ({ visible}) => {
+  const { data } = useCurrentUser();
   if (!visible) return null
   return (
     <div className="bg-black w-56 absolute top-14 right-0 py-5 flex-col border-2 border-gray-800 flex">
@@ -13,7 +15,7 @@ const AccountMenu: React.FC<AccountMenuProps> = ({ visible}) => {
         <div className="px-3 group/item flex flex-row gap-3 items-center w-full">
           <img className="w-8 rounded-md" src="/images/default-blue.png" alt=""/>
           <p className="text-white text-sm group-hover/item:underline">
-            Username
+            {data?.name}
           </p>
         </div>
         <hr className="bg-gray-600 border-0 h-px my-4"/>
