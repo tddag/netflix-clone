179de3952c0a60385c27470ba560322c178d8844
Tam Dang <danhtam0509@gmail.com>
2024-11-25 00:55:11 -0500

    update

--- lib/prismadb.ts
+++ lib/prismadb.ts
@@ -1,6 +1,10 @@
 import { PrismaClient } from '@prisma/client';
-
+console.log("PrismaDB-TD: ")
+console.log(global)
 const client = global.prismadb || new PrismaClient();
 if (process.env.NODE_ENV !== 'production') global.prismadb = client;
+
+// console.log("PrismaDB-TD2: ")
+// console.log(global)
 
 export default client;
